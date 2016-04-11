65aaff0625b05483e2491e3066cac3c7ea34b52a
LadyVamp <ladyvamp666@gmail.com>
2016-04-11 11:55:41 +0300

    contact information

--- views/site/contact.php
+++ views/site/contact.php
@@ -3,8 +3,23 @@
 <section>
     <div class="container">
         <div class="row">
+            
+            <div class="col-sm-6 padding-right contact-information">   
+                <h4>Контактная информация</h4>
+                <b>Единый справочный телефон:</b> +7 (495) 925-6473 <br>
+                <b>E-mail:</b> alaptopshop@gmail.com<br>
+                <b>Адрес магазина:</b> 127642, г.Москва, ул.Сухонская, д.7в<br><br>
+                <b>График работы:</b><br>
+                понедельник-пятница с 10:00 до 21:00 <br>
+                суббота, воскресенье с 10:00 до 20:00 <br> 
+                
+                <div class="contact-information-map">              
+                <b>Как проехать</b><br>
+                <img src="../template/images/home/map.jpg" alt="схема проезда"> 
+                </div>
+            </div>
 
-            <div class="col-sm-4 col-sm-offset-4 padding-right">
+            <div class="col-sm-4 padding-right">
 
                 <?php if ($result): ?>
                     <p>Сообщение отправлено! Мы ответим Вам на указанный email.</p>
@@ -36,6 +51,14 @@
                 <br/>
                 <br/>
             </div>
+            
+<!--            <div class="col-sm-6 padding-right contact-information-map">              
+                <b>Как проехать</b>
+                <img src="../template/images/home/map.jpg" alt="схема проезда"> 
+            </div>-->
+
+            
+          
         </div>
     </div>
 </section>
