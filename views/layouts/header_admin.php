118aabb6b9ba5ae3454da1d52c0636ff660621e0
LadyVamp <ladyvamp666@gmail.com>
2016-05-08 20:32:16 +0300

    adminpanel: add favicon

--- views/layouts/header_admin.php
+++ views/layouts/header_admin.php
@@ -18,11 +18,7 @@
         <script src="js/html5shiv.js"></script>
         <script src="js/respond.min.js"></script>
         <![endif]-->       
-        <link rel="shortcut icon" href="/template/images/ico/favicon.ico">
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/template/images/ico/apple-touch-icon-144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/template/images/ico/apple-touch-icon-114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/template/images/ico/apple-touch-icon-72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="/template/images/ico/apple-touch-icon-57-precomposed.png">
+        <link rel="shortcut icon" href="/template/images/home/admin-favicon.ico">
     </head><!--/head-->
 
     <body>
