83d4adebca9fbd73cdc83c02e2e58995ec7c5165
LadyVamp <ladyvamp666@gmail.com>
2016-05-26 15:55:57 +0300

    add links in table

--- views/site/choice.php
+++ views/site/choice.php
@@ -9,9 +9,9 @@
     <section>
     <div class="container">
         <div class="row">
-            <div class="col-sm-3"></div>   
-            <div class="col-sm-6 padding-right choice">   
-                <h3>Подбор ноутбука</h3>                                                     
+<!--            <div class="col-sm-3"></div>   
+            <div class="col-sm-6 padding-right choice">   -->
+                <h2>Подбор ноутбука</h2>                                                     
                      <div id='options'>
                         <form name="form" method="post" class="choice">
                             <!--<div class="slider_price"> Декоративный слайдер!!!
@@ -24,8 +24,8 @@
                                     </label>
                                     <div id="slider_price"></div>
                              </div> -->
-                            
-                             
+                            <div class="col-sm-1"></div>
+                            <div class="col-sm-5 padding-right choice">
                                 <div class="price">
                                     <h4>Цена, руб:</h4>
                                         <label>От: </label><input type="text" name="price_start" placeholder="16400">
@@ -44,42 +44,55 @@
                                         <li><input id="b7" type="checkbox" name="brands[]" value="7" hidden /> <label for="b7">MSI</label></li>
                                     </ul>	
                                  </div>
+                             </div>
                             
-                            <div class="CPU">
-                                <h4>Частота процессора, Мгц:</h4>
-                                    <label>От: </label><input type="text" name="cpu_start" placeholder="800">
-                                    <label>До: </label><input type="text" name="cpu_end" placeholder="3100"> 
-                            </div>
-                            
-                            <div class="hard_disk">
-                                <h4>Объем жесткого диска, Гб:</h4>
-                                    <label>От: </label><input type="text" name="hard_start" placeholder="32">
-                                    <label>До: </label><input type="text" name="hard_end" placeholder="2000"> 
-                            </div>
-                            
-                            <div class="RAM">
-                                <h4>Объем оперативной памяти, Гб:</h4>
-                                    <label>От: </label><input type="text" name="ram_start" placeholder="2">
-                                    <label>До: </label><input type="text" name="ram_end" placeholder="16"> 
-                            </div>
-                            
-                            <div class="weight">
-                                <h4>Вес, кг:</h4>
-                                    <label>От: </label><input type="text" name="weight_start" placeholder="1">
-                                    <label>До: </label><input type="text" name="weight_end" placeholder="3.5"> 
-                            </div>
+                            <div class="col-sm-5 padding-right choice">
+                                <div class="CPU">
+                                    <h4>Частота процессора, Мгц:</h4>
+                                        <label>От: </label><input type="text" name="cpu_start" placeholder="800">
+                                        <label>До: </label><input type="text" name="cpu_end" placeholder="3100"> 
+                                </div>
+
+                                <div class="hard_disk">
+                                    <h4>Объем жесткого диска, Гб:</h4>
+                                        <label>От: </label><input type="text" name="hard_start" placeholder="32">
+                                        <label>До: </label><input type="text" name="hard_end" placeholder="2000"> 
+                                </div>
+
+                                <div class="RAM">
+                                    <h4>Объем оперативной памяти, Гб:</h4>
+                                        <label>От: </label><input type="text" name="ram_start" placeholder="2">
+                                        <label>До: </label><input type="text" name="ram_end" placeholder="16"> 
+                                </div>
+
+                                <div class="weight">
+                                    <h4>Вес, кг:</h4>
+                                        <label>От: </label><input type="text" name="weight_start" placeholder="1">
+                                        <label>До: </label><input type="text" name="weight_end" placeholder="3.5"> 
+                                </div>
+                                <div class="battery_life">
+                                    <h4>Время автономной работы, ч:</h4>
+                                        <label>От: </label><input type="text" name="battery_start" placeholder="3">
+                                        <label>До: </label><input type="text" name="battery_end" placeholder="12"> 
+                                </div>
+                              </div>  
+                            <div class="col-sm-1"></div>
+
                             
 <!--                                <ul>
                                     <li><label><input type="checkbox" name="availability">В наличии</label></li>
                                     <li><label>Хит продаж</label><input type="checkbox" name="is_hit" value="2"></li>
                                 </ul>-->
-                            <div class="choice-btn">       
-                                <input type="submit" name="filter" value="Подобрать" class="btn btn-default" />
+
+                            <div class="row">
+                                <div class="col-sm-10 choice-btn center">       
+                                    <input type="submit" name="filter" value="Подобрать" class="btn btn-default" />
+                                </div>
                             </div>
-              </form>
+                        </form>
                     </div>
-            <div class="col-sm-3"></div>   
-        </div>
+            <!--<div class="col-sm-3"></div>-->   
+        <!--</div>-->
     </div> <!--/row-->
     
     <div class="row">
@@ -91,7 +104,7 @@
             $username = 'root';
             $passwordname = '';
             $basename = 'myshop';
-            $conn = new mysqli($hostname, $username, $passwordname, $basename) or die       ('Невозможно открыть базу');
+            $conn = new mysqli($hostname, $username, $passwordname, $basename) or die ('Невозможно открыть базу');
    
 
             function addWhere($where, $add, $and = true) {
@@ -110,19 +123,23 @@
               if ($_POST["cpu_end"]) $where = addWhere($where, "`CPU` <= '".htmlspecialchars($_POST["cpu_end"]))."'";
               if ($_POST["hard_start"]) $where = addWhere($where, "`hard_disk` >= '".htmlspecialchars($_POST["hard_start"]))."'";
               if ($_POST["hard_end"]) $where = addWhere($where, "`hard_disk` <= '".htmlspecialchars($_POST["hard_end"]))."'";
-              if ($_POST["ram_start"]) $where = addWhere($where, "`ram_disk` >= '".htmlspecialchars($_POST["ram_start"]))."'";
-              if ($_POST["ram_end"]) $where = addWhere($where, "`ram_disk` <= '".htmlspecialchars($_POST["ram_end"]))."'";
+              if ($_POST["ram_start"]) $where = addWhere($where, "`RAM` >= '".htmlspecialchars($_POST["ram_start"]))."'";
+              if ($_POST["ram_end"]) $where = addWhere($where, "`RAM` <= '".htmlspecialchars($_POST["ram_end"]))."'";
+              if ($_POST["battery_start"]) $where = addWhere($where, "`battery_life` >= '".htmlspecialchars($_POST["battery_start"]))."'";
+              if ($_POST["battery_end"]) $where = addWhere($where, "`battery_life` <= '".htmlspecialchars($_POST["battery_end"]))."'";
               if ($_POST["brands"]) $where = addWhere($where, "`brand_id` IN (".htmlspecialchars(implode(",", $_POST["brands"])).")");
 //              if ($_POST["availability"]) $where = addWhere($where, "`availability` = '1'");
               $sql = "SELECT * FROM `product`";
               
-//              $res = mysql_query($sql) or trigger_error(mysql_error()." in ".$sql); //показ ошибок
+              //$res = mysql_query($sql) or trigger_error(mysql_error()." in ".$sql); //показ ошибок
 
               if ($where) $sql .= " WHERE $where";
-              echo $sql;
+              //echo $sql; //показ sql-запроса
 
                 $result = $conn->query($sql); 
-   
+                if (!$result) echo '<span class="red-msg">Ни один ноутбук не удовлетворяет условиям. Измените параметры подбора </span>';
+                
+              echo '<div class="space"></div>';  
               echo '<h3>Список подобранных ноутбуков</h3>';   
               echo '<table class="table-bordered table-striped table">';
              echo '<tr>';
@@ -131,17 +148,19 @@
                 echo '<th>Объем ОП</th>';
                 echo '<th>Объем жесткого диска</th>';
                 echo '<th>Вес</th>';
+                echo '<th>Время автономной работы</th>';
                 echo '<th>Цена</th>';
                 echo '</tr>';
 
                 while ($data = $result->fetch_assoc())
                 {
                    echo '<tr>';
-                        echo '<td>' . $data['name'] . '</td>';
+                        echo '<td>'. '<a title="Перейти к описанию ноутбука" target="_blank"  href=/product/'. $data["id"] . '>'. $data["name"] . '</a>' . '</td>';
                         echo '<td>' . $data['CPU'] . '</td>';
                         echo '<td>' . $data['RAM'] . '</td>';
                         echo '<td>' . $data['hard_disk'] . '</td>';
                         echo '<td>' . $data['weight'] . '</td>';
+                        echo '<td>' . $data['battery_life'] . '</td>';
                         echo '<td>' . $data['price'] . '</td>';
                   echo '</tr>';
                 }
